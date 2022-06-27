4655ac866a05ef9d117e179d6630fd6b18cb17c8
qaAutomations <107888756+qaAutomations@users.noreply.github.com>
2022-06-28 01:56:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //1
+        Scanner scanner = new Scanner(System.in);
+        // System.out.println("please enter your name");
+        // String name = scanner.nextLine();
+        // System.out.println("please enter your age");
+        // int age = scanner.nextInt();
+        // if (age>50){
+        //     System.out.println(name.toUpperCase());
+//
+        // }else{
+        //     System.out.println(name.toLowerCase());
+        //}
+
+        //2
+
+        //System.out.println("please enter a number : ");
+        //int number2 = scanner.nextInt();
+        //number2 = number2%10;//ahadoooot
+        //System.out.println(number2);
+
+        //3
+       //System.out.println("please enter a day");
+       //int day= scanner.nextInt();
+
+       //switch (day%7){
+       //    case 0 -> System.out.println("sat");
+       //    case 1 -> System.out.println("sun");
+       //    case 2 -> System.out.println("mon");
+       //    case 3 -> System.out.println("tue");
+       //    case 4 -> System.out.println("wed");
+       //    case 5 -> System.out.println("thu");
+       //    case 6 -> System.out.println("fri");
+       //}
+
+
+
+    }
+}
---
+++ lolo.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+
+public class lolo {
+    public static void main(String[] args) {
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        //System.out.println("joey doesn't share food ");
+        // Scanner scanner = new Scanner(System.in);
+//
+//
+        // for (int i = 1 ; i <= 10 ; i+=2 ){
+        //     System.out.println(i);
+        // }
+        // for (int i = 1 ; i <= 10 ; i++ ){
+        //     if (i%2 ==1)
+        //         System.out.println(i);
+        // }
+
+        //  for (int i = 10 ; i < 20; i++) {
+        //         System.out.println(i);
+        // }
+
+        //for(int i = 20 ; i<=40;i++){
+        //    System.out.println(i);
+        //}
+
+        //for (int i = 0; i < 10000; i++) {
+        //    if (i % 100 == 0)
+        //        System.out.println(i);
+        //}
+        //for (int i = 0; i < 10000; i += 100) {
+        //    System.out.println(i);
+        //}
+
+        Scanner scanner = new Scanner(System.in);
+        // System.out.println("please enter a number :");
+        // int a = scanner.nextInt();
+//
+        // for (int i = 0; i<a ; i++){
+        //     System.out.println("DAMN BAM MAM OO LALALA");
+        // }
+//
+      // int x = 84;
+      // int newNumber = 0;
+      // int ahadot=0;
+      // for (int i=x; i > 0 ;i=i/10) {
+      //     newNumber = newNumber * 10;
+      //     ahadot = x % 10;
+      //     x = x / 10;
+      //     newNumber = newNumber + ahadot;
+      // }
+      // System.out.println(newNumber);
+
+
+//
+      //  System.out.println("please enter a number :");
+      //  int x = scanner.nextInt();
+//
+      //  while(x != 50){
+      //      System.out.println("please enter a number :");
+      //      x = scanner.nextInt();
+      //  }
+      //  System.out.println("Done");
+
+
+        //  1
+
+      //int b= 5;
+      //while(b <= 75){
+      //    System.out.println(b);
+      //    b++;
+      //}
+
+
+      System.out.println("please enter a number :");
+        int x = scanner.nextInt();
+        int counter= 0;
+
+          while(x != 133){
+              System.out.println("please enter a number :");
+              x = scanner.nextInt();
+              counter++;
+          }
+          System.out.println("Done"+counter);
+
+
+
+          int c = 1;
+          int sum =0;
+        while(c <=10){
+            sum = sum +c;
+            c++;
+        }
+        System.out.println(sum);
+
+      //  5! = 1*2*3*4*5
+    }
+}
